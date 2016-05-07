3770ce526ead88adeb9f1e8534d1130f36284dad
Marcel Michel <poissonpourri@gmail.com>
2016-05-07 16:35:42 -0400

    ajout du paiment paypal

---
+++ GrosBrasInc/GrosBrasInc/Controllers/PaypalController.cs
@@ -0,0 +1,77 @@
+﻿using GrosBrasInc.Models;
+using PayPal.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GrosBrasInc.Controllers
+{
+    [Authorize]
+    public class PaypalController : Controller
+    {
+        // GET: Paypal
+        public ActionResult Index()
+        {
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            if (cart == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            var ls = cart.GetCartItems();
+            var _total = cart.GetTotal();
+            var config = ConfigManager.Instance.GetProperties();
+            var accessToken = new OAuthTokenCredential(config).GetAccessToken();
+            var apiContext = new APIContext(accessToken);
+
+            var payment = Payment.Create(apiContext, new Payment
+            {
+                intent = "sale",
+                payer = new Payer
+                {
+                    payment_method = "paypal"
+                },
+                transactions = new List<Transaction>
+                {
+                    new Transaction
+                    {
+                        description = "Transaction description.",
+                        invoice_number = "001",
+                        amount = new Amount
+                        {
+                            currency = "CAD",
+                            total = (_total + 25).ToString(),
+                            details = new Details
+                            {
+                                tax = "15",
+                                shipping = "10",
+                                subtotal = _total.ToString()
+                            }
+                        },
+                        item_list = new ItemList
+                        {
+                            items = ls.Select(x => new Item {
+                                sku = x.Article.ArticleID.ToString(),
+                                price = x.Article.Prix.ToString(),
+                                name = x.Article.NomArticle.ToString(),
+                                quantity = x.Count.ToString(),
+                                description = x.Article.Description,
+                                currency = "CAD",
+                                tax = (x.Article.Prix * 0.15).ToString()
+                            }).ToList()
+                        }
+                    }
+                },
+                redirect_urls = new RedirectUrls
+                {
+                    return_url = "http://localhost:38716/Checkout/Complete",
+                    cancel_url = "http://localhost:38716/Panier"
+                }
+            });
+
+            return this.Redirect(payment.GetApprovalUrl());
+        }
+    }
+}
